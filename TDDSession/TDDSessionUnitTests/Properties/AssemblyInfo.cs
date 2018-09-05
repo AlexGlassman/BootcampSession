4e56b8b3b3cb84a14be74281c8eb7d5dca0339f2
Alex Glassman <alex.glassman@asos.com>
2018-09-05 22:22:05 +0100

    added test

---
+++ TDDSession/TDDSessionUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TDDSessionUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TDDSessionUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0cf9ddd-56c3-4b9f-9944-c3ceac39dcc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TDDSession/TDDSessionUnitTests/UnitTestExampleShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDSessionUnitTests
+{
+    [TestFixture]
+    public class UnitTestExampleShould
+    {
+
+        [Test]
+        public void ReturnTrue()
+        {
+            var expected = true;
+            var actual = true;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
