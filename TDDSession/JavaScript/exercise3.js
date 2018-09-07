6a29152db840ca2048af15c99253853231079690
James Norton <jamesn@asos.com>
2018-09-07 16:32:28 +0100

    adding JS exercise 3 example

---
+++ TDDSession/JavaScript/exercise3.js
@@ -0,0 +1,20 @@
+class DataStructureCreator {
+	constructor() {
+		this.values = {};
+	}
+	setNumberValue(num) {
+		this.values.number = num;
+	}
+	setStringValue(str) {
+		this.valus.string = str;
+	}
+	setDateValue(dte) {
+
+	}
+	setArrayValue(arr) {
+
+	}
+	getAllValues() {
+		return this.values;
+	}
+}
---
+++ TDDSession/JavaScript/exercise3assertions.js
@@ -0,0 +1,7 @@
+expect(o.getAllValues()).toEqual({
+	{
+		string: 'sdfs',
+		number: 2
+		date: 
+	}
+})
